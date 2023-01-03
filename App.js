2706ed8f20a1dbc5c67cc5e3e848148d604a3d15
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2023-01-03 15:40:25 +0100

    chore: move the assets file

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar as MobileStatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>dsjdjhsd</Text>
+        <Text>Open up App.js to start worki justbhbshdbs on your app! to see dd</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#fff4ac",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? MobileStatusBar.currentHeight : 0,
   },
 });
